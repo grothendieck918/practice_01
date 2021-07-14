33cde10137e8bed74a0a01374132315b7d0ddecf
kangt <kangtor918@gmail.com>
2021-07-14 09:42:47 +0900

    2021.07.14 1ban practice 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from accountapp.views import hello_world, AccountCreateView
@@ -22,7 +23,8 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),  # 두번째 인자는 함수
-    
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create')  # 두번째인자가 클래스이므로 as_view()를써서 함수처럼
 
 ]
