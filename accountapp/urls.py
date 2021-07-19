46f0106ef1b13ae295587101cb7502b670ac35cf
kangt <kangtor918@gmail.com>
2021-07-19 14:51:11 +0900

    2021.07.19 1ban practice 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -28,5 +28,6 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),  # 두번째인자가 클래스이므로 as_view()를써서 함수처럼
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 
 ]
