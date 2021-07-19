464df477ccea1a5872e91f2b06286e6e5e110943
kangt <kangtor918@gmail.com>
2021-07-19 15:40:52 +0900

    2021.07.19 1ban practice 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
 
@@ -44,7 +45,7 @@ class AccountDetailView(DetailView):
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
-    form_class = UserCreationForm
+    form_class = AccountCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
