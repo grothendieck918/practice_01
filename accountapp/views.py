3c7a6582391c49085be68a65de29d35c38cdd68b
kangt <kangtor918@gmail.com>
2021-07-28 10:39:20 +0900

    2021.07.28 1ban practice 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -38,8 +38,11 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 class AccountDetailView(DetailView):
     model = User
@@ -55,8 +58,11 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 @method_decorator(has_decorator, 'get')
 @method_decorator(has_decorator, 'post')
