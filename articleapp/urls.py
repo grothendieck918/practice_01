c6ce21ce2e5e9b9c42bb5b7ec7cf420ab7b40209
kangt <kangtor918@gmail.com>
2021-07-29 15:19:26 +0900

    2021.07.29 1ban practice 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+
+# app_name = 'articleapp'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls')),
-    path('profiles/', include('profileapp.urls')),
-    path('articles/', include('articleapp.urls'))
-] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
