3fb0efdcadbb58c0a159746b2333b3847e248a7b
kangt <kangtor918@gmail.com>
2021-07-29 15:54:04 +0900

    2021.07.29 1ban practice 4

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
-# app_name = 'articleapp'
+app_name = 'articleapp'
 
 urlpatterns = [
-
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'),name='list'),
 ]
