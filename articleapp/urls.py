4b534bed6e27bab675f8c5c02e4c5d937910b4a9
kangt <kangtor918@gmail.com>
2021-08-04 09:48:28 +0900

    2021.08.4 1ban practice 2

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
-from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = 'articleapp'
 
@@ -28,4 +28,5 @@ urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
     path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
 ]
